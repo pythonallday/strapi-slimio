7d249cf1b1a1d9defe460da8e0453f2baaf49c0b
pythonallday <aisaev@live.nl>
2025-12-04 16:02:22 +0100

    added cta content type

---
+++ src/api/call-to-action/controllers/call-to-action.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * call-to-action controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::call-to-action.call-to-action');
+
---
+++ src/api/call-to-action/services/call-to-action.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * call-to-action service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::call-to-action.call-to-action');
+
